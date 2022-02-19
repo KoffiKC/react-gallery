a8a63722d24e69a48c5bff9a57c6f660f561daf0
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-02-18 20:55:57 -0600

    GET requests successfully retreives data from server on load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import React from 'react';
 import './App.css';
 
+
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  //On load, get guests
+  useEffect(() => {
+    getPics()
+  }, [])
+
+  // GET
+  const getPics = () => {
+    console.log('oo now im in getting pics');
+
+    axios.get('/gallery')
+      .then(response => {
+        console.log('we got all them pics');
+        //RENDER FUNCTION HERE
+      }).catch(err => {
+        console.log('camera malfunction :/', err);
+      })
+  }
+
+
+
+  // POST oh wait
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
